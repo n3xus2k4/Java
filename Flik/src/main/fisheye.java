c824813be734e7dfcbbfcae96a7bdad79a0e4255
n3xus2k4 <nexus@coki.eu>
2012-01-09 11:34:22 +0100

    Neues Auge!

---
+++ Flik/src/main/fisheye.java
@@ -0,0 +1,12 @@
+package main;
+
+public class fisheye {
+
+	public fisheye () {
+		System.out.println("Fischeaugen sehen alles!");
+	}
+	
+	public void addEye() {
+		System.out.println("Eye added!");
+	}
+}
